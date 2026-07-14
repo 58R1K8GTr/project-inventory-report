f65a614735db02efd125057c394ce0591b89c9f4
no_name <no@email.com>
2026-07-14 16:39:16 -0300

    requisito 5 satisfeito

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,13 @@
+from .product import Product
+
+
 class Inventory:
-    pass
+    def __init__(self, data: list[Product] | None = None) -> None:
+        self._data = data or []
+
+    @property
+    def data(self) -> list[Product]:
+        return self._data[:]
+
+    def add_data(self, data: list[Product]) -> None:
+        self._data += data
